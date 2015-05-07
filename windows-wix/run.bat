392da698272aa602c1452eb393a1d52e22aaf95e
marz <marzwu@gmail.com>
2015-05-07 22:56:34 +0800

    add run script

---
+++ windows-wix/run.bat
@@ -0,0 +1,7 @@
+set ZEAL_VERSION=0.1.0
+set ZEAL_BIN_DIR=F:\Qt\Qt5.4.1-mingw\5.4\mingw491_32\bin
+set QTDIR=F:\Qt\Qt5.4.1-mingw\5.4\mingw491_32
+set QTTOOLSDIR=F:\Qt\Qt5.4.1-mingw\Tools\mingw491_32
+
+candle zeal.wxs
+light -ext WixUIExtension zeal.wixobj
